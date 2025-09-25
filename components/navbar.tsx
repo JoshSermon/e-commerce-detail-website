175b155de0bf09b5bf2745d1919b4461bff85301
Joshua Sermon <JoshuaSermon1@gmail.com>
2025-09-25 14:41:45 -0500

    More styling to Navabar - react

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export const Navbar = () => {
   return (
-  <nav>
+  <nav className="sticky top-0 z-50 bg-white shadow">
     <div className="container mx-auto flex items-center justify-between px-4 py-4">
         <Link href="/" className="flex items-center space-x-2">
             Arvo
