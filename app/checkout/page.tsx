4c57d2f2abc9bbc3d3a408246dbd916e5a510aed
Joshua Sermon <JoshuaSermon1@gmail.com>
2025-09-30 21:25:53 -0500

    Updating checkout page

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,3 +1,7 @@
 export default function CheckoutPage() {
-    return <div className="div">Checkout</div>
+        return (
+      <div className="container mx-auto px-4 py-8 text-center">
+        <h1 className="text-3xl font-bold mb-4">Your Cart is Empty</h1>
+      </div>
+    );
 }
