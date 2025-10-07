35ea9ed867540e64aa0c86f5fe5deed92e4892b7
Joshua Sermon <JoshuaSermon1@gmail.com>
2025-10-06 22:44:06 -0500

    Updating checkout page - part 4

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div>
             <section className="rounded bg-neutral-100 py-8 sm:py-12">
                 <h2 className="text-3xl font-bold tracking-tight md:text-4xl">
-                    Welcome to My Arvo
+                    Welcome to My Arvo! More tha shopping.
                 </h2>
             </section>
     </div>
