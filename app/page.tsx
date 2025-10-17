e845c50e29d258754a2841de64561f8e42c214d7
Joshua Sermon <JoshuaSermon1@gmail.com>
2025-10-17 14:53:46 -0500

    Updating Main Page and Creating Button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,28 @@
 import Image from "next/image";
+import styles from "./page.module.css";
+import { stripe } from "@/lib/stripe";
 
-export default function Home() {
+export default async function Home() {
+  const products = await stripe.products.list({
+    expand: ["data.default_price"],
+    limit: 5,
+  });
+
+  console.log(products);
+
   return (
     <div>
             <section className="rounded bg-neutral-100 py-8 sm:py-12">
-                <h2 className="text-3xl font-bold tracking-tight md:text-4xl">
-                    Welcome to My Arvo!
-                </h2>
+              <div className="div">
+                <div className="div">
+                    <h2 className="text-3xl font-bold tracking-tight md:text-4xl">
+                        Welcome to Arvo!
+                    </h2>
+                    <p> 
+                        Creating and delivering high quality digital products for you.
+                    </p>
+                  </div>
+              </div>
             </section>
     </div>
   );
