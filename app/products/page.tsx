0eac4f49b3b4d55dc4fab24b04125349549fcac7
Joshua Sermon <JoshuaSermon1@gmail.com>
2025-10-21 20:01:59 -0500

    Updating main page - small update

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -3,7 +3,7 @@ export default function ProductsPage() {
   return (
     <div className="pb-8">
       <h1 className="text-3xl font-bold leading-none tracking-tight text-foreground text-center mb-8">
-        All Products Here
+        All Products Here! Shop Now.
       </h1>
     </div>
   );
