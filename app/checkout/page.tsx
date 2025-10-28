189684e0278565fe158661656c7d93ca3767de06
Joshua Sermon <JoshuaSermon1@gmail.com>
2025-10-27 23:08:43 -0500

    Updating main page - small update three

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,7 +1,7 @@
 export default function CheckoutPage() {
         return (
       <div className="container mx-auto px-4 py-8 text-center">
-        <h1 className="text-3xl font-bold mb-4">Your Cart is Full. Replace now.</h1>
+        <h1 className="text-3xl font-bold mb-4">Your Cart is Full.</h1>
       </div>
     );
 }
