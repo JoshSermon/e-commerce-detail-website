376e4c77d9d61d63379ac480ec29ed43cf02a448
Joshua Sermon <JoshuaSermon1@gmail.com>
2025-10-29 14:48:43 -0500

    Updating carousel

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -1,11 +1,20 @@
 import Stripe from "stripe";
 import { Card } from "@/components/ui/card";
+import { useEffect, useState } from "react";
 
 interface Props {
-    products: Stripe.Product
+    products: Stripe.Product[];
 }
 
 export const Carousel = ({products}: Props) => {
+    const [current, setCurrent] = useState<number>(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrent((prev) => (prev + 1) % products.length);
+        }, 3000);
+    }, [products.length]);
+    
     return <Card>
 
     </Card>;
