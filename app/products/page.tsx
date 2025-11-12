3879c5e44c35fb77cdad8ad66519b3aaa5c0a763
Joshua Sermon <JoshuaSermon1@gmail.com>
2025-11-12 14:49:44 -0600

    Adding product feature section - part three

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -10,7 +10,7 @@ export default async function ProductsPage() {
   return (
     <div className="pb-8">
       <h1 className="text-3xl font-bold leading-none tracking-tight text-foreground text-center mb-8">
-        All Products Here!
+        All Products Here! Shop Now
       </h1>
     </div>
   );
